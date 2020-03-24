8a409c9477305644d538dfd3ed1490925e71e6b0
Sanjay Gubaju <8557287+SanjayGubaju@users.noreply.github.com>
2020-03-24 21:39:22 +0545

    cleaned signal emit method

--- presets/companion/cat/cat.gd
+++ presets/companion/cat/cat.gd
@@ -6,8 +6,7 @@ var current_node
 var direction = 1
 var animation
 
-var collision = false;
-var canCry = false
+var interact = false
 
 func _ready():
 	set_process(true)
@@ -17,18 +16,14 @@ func _ready():
 func _physics_process(delta):
 	# Follow player when inside hit area.
 	var current_body = get_node("Area2D").get_overlapping_bodies()
-
-	collision = false;
 	
 	if(current_body.size() != 0):
 		for tinge in current_body:
 			if(tinge.is_in_group("group_player")):
 				
-				collision = true
-				
 				if Input.is_action_just_pressed("ui_up"):
-					canCry = true
-					
+					interact = true
+				
 				if(tinge.get_global_position().x < self.get_global_position().x - 16):
 					direction = 0
 				if(tinge.get_global_position().x > self.get_global_position().x + 16):
@@ -40,12 +35,17 @@ func _physics_process(delta):
 		current_node.set_flip_h(true)
 
 	if !animation.is_playing():
-		if not canCry:
+		if not interact:
 			animation.play("idle")
 		else:
-			canCry = false
-			
-			if collision: 
-				animation.play("stretch")
-			
-	emit_signal("control", 'e', collision, position)
+			animation.play("stretch")
+			interact = false
+
+func _on_Area2D_body_shape_entered(body_id, body, body_shape, area_shape):
+	if(body.is_in_group("group_player")):
+		emit_signal("control", 'e', true, position)
+		
+func _on_Area2D_body_shape_exited(body_id, body, body_shape, area_shape):
+	if(body.is_in_group("group_player")):
+		interact = false
+		emit_signal("control", 'e', false, position)
